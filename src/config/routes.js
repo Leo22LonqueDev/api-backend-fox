44db6b7531d543c8cd1bcb6a635b0486df9f57a9
Leo22LonqueDev <leonardoarrudalonque@outlook.com>
2024-03-08 20:47:19 -0300

    Update leonardo veiculos

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,10 +3,18 @@ const publicController = require('../controllers/publicController')
 const userController = require('../controllers/userController')
 const router = express.Router()
 
+//Arquivo de rotas
+
+const veiculosRoutes = require('./routes/veiculos')
+
 //Public routes
 
 router.get('/', publicController.index)
 router.post('/users', userController.create)
 router.get('/users', userController.index)
+
+//Rotas das celulas
+
+router.use('/veiculos', veiculosRoutes)
 
 module.exports = router
