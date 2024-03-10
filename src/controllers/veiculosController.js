3ea9a6f8c70f70b97e356e24dea2945366384016
gabrielGO1 <gabrielprogo@hotmail.com>
2024-03-10 15:38:30 -0300

    gabriel

--- src/controllers/veiculosController.js
+++ src/controllers/veiculosController.js
@@ -7,7 +7,7 @@ module.exports = {
             const { modelo, placa, anoDeFabricacao, cor, marca } = req.body
 
             console.log(req.body);
-            
+
             const newVeiculo = await Veiculos.create({
                 modelo,
                 placa,
@@ -28,7 +28,7 @@ module.exports = {
     },
 
     getVeiculos: async (req, res) => {
-        try {   
+        try {
             const result = await Veiculos.find()
             return res.status(200).json(result)
         } catch (error) {
@@ -37,6 +37,39 @@ module.exports = {
                 msg: 'Internal Server Error',
                 error: error.message
             });
+        }
+    },
+
+    updateVeiculos: async (req, res) => {
+        try {
+            const { modelo, placa, anoDeFabricacao, cor, marca } = req.body
+            await Veiculos.updateOne({ _id: req.body.id }, { modelo, placa, anoDeFabricacao, cor, marca })
+
+            return res.status(200).json({ msg: "OK" })
+
+        } catch (error) {
+            return res.status(500).json({
+                msg: 'Internal Server Error',
+                error: error.message
+            });
+
+        }
+    },
+    filterVeiculos: async (req, res) => {
+        try {
+            const { modelo } = req.body
+            const filter = await Veiculos.find({
+                modelo: { $regex: new RegExp(modelo, "i") }
+            })
+            console.log(filter)
+            return res.status(200).json({filter})
+
+        } catch (error) {
+            return res.status(500).json({
+                msg: 'Internal Server Error',
+                error: error.message
+            });
+
         }
     }
 }
