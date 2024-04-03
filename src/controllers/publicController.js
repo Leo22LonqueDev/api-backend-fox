e86566bdb8f4b57f07c8cce7ba28c71c1c71c477
Leo22LonqueDev <leonardoarrudalonque@outlook.com>
2024-04-02 23:19:10 -0300

    Update leonardo

--- src/controllers/publicController.js
+++ src/controllers/publicController.js
@@ -10,26 +10,24 @@ module.exports = {
     },
 
     login: async (req, res) => {
-        const { email, senha } = req.body
-        
-        console.log(req.body);
+        const { email, password } = req.body
 
-        if (!email || !senha) return res.status(400).json({ message: "Necessário preencher todos os campos" })
+        if (!email || !password) return res.status(400).json({ message: "Necessário preencher todos os campos" })
 
         try {
             const user = await User.findOne({ email: email }).lean()
 
             if (!user) return res.status(404).json({ message: `Usuario ou senha incorretos` })
 
-            const checkSenha = await bcrypt.compare(senha, user.senha)
+            const checkPassword = await bcrypt.compare(password, user.password)
 
-            if (!checkSenha) return res.status(422).json({ message: `Usuario ou senha incorretos` })
+            if (!checkPassword) return res.status(422).json({ message: `Usuario ou senha incorretos` })
 
             //Criando token de autenticação
-            const values = jwt.sign({ username: user.name, email: email, accessLevel: user.accessLevel }, secret)
+            const token = jwt.sign({ username: user.nome, email: email, accessLevel: user.accessLevel }, secret)
 
             //Setando o token para o cookie
-            return res.status(200).json({ msg: "Logado com sucesso", values: values, user: user.name })
+            return res.status(200).json({ msg: "Logado com sucesso", user: user.nome })
         } catch (error) {
             return res.json(error)
         }
