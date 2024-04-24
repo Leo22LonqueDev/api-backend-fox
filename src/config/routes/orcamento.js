6f1b5aca18aca1f61666089ea5f0b9a7ddea1f7f
Leo22LonqueDev <leonardoarrudalonque@outlook.com>
2024-04-23 22:24:50 -0300

    Update leonardo

--- src/config/routes/orcamento.js
+++ src/config/routes/orcamento.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const orcamentoController = require('../../controllers/orcamentoController')
 const router = express.Router()
 
 router.post('/createOrcamento', orcamentoController.createOrcamento)
